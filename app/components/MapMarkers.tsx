04302a9a092231711ddf50516992efb081104bc8
Ifeanyi Dike <ifeanyidike87@gmail.com>
2023-05-29 17:11:19 +0100

    declare google as const

--- app/components/MapMarkers.tsx
+++ app/components/MapMarkers.tsx
@@ -2,19 +2,19 @@ import type { MapLocations } from "@component/utils/types";
 import { useGoogleMap } from "@ubilabs/google-maps-react-hooks";
 import { useEffect, useRef } from "react";
 
+declare const google: any;
 const MapMarkers = ({ mapLocations }: { mapLocations: MapLocations[] }) => {
   const map = useGoogleMap();
   const markersRef = useRef<any>([]);
 
   useEffect(() => {
-    if (!map || markersRef.current.length !== 0 || !window || !window?.google)
-      return;
+    if (!map || markersRef.current.length !== 0) return;
 
     markersRef.current = mapLocations
       .slice(0, 5)
       .map(
         (location: MapLocations) =>
-          new window.google.maps.Marker({ map, position: location })
+          new google.maps.Marker({ map, position: location })
       );
   }, [map]);
 
